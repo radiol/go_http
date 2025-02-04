8f96c2f253ef040d2dc9e8368ad8835b68a9c01a
radiol <radiol@github.com>
2025-02-04 13:28:34 +0900

    HTTP request:http://httpbin.org/ip/

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	base, err := url.Parse("https://httpbin.org/")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ref, err := url.Parse("/ip")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	endpoint := base.ResolveReference(ref).String()
+	fmt.Println(endpoint)
+
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
