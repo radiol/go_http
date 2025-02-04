6114a14514c0f2cdbf855a923d87233a2cd4bb7b
radiol <radiol@github.com>
2025-02-04 14:29:53 +0900

    Add HTTP client and perform POST request

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -32,4 +34,46 @@ func main() {
 	fmt.Println(q)
 	// & is escaped to %26, % is escaped to %25
 	fmt.Println(q.Encode())
+
+	// HTTPクライアントを作成
+	var client *http.Client = &http.Client{}
+	// リクエストを送信
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
+
+	// POST request
+	ref, err = url.Parse("/status")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	endpoint = base.ResolveReference(ref).String()
+	fmt.Println(endpoint)
+	req, err = http.NewRequest("POST", endpoint, bytes.NewBuffer([]byte("test")))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
